88d5f03173907cece8ea0233f233e4b6ff316bfe
kavirajdesai <97800048+kavirajdesai@users.noreply.github.com>
2022-02-03 13:47:05 +0530

    Add files via upload

---
+++ PRAC_5_SWAPCASE.py
@@ -0,0 +1,11 @@
+""" KAVIRAJ DESAI 20CE023
+You are given a string and your task is to swap cases. In other words, convert all lowercase letters to uppercase letters and vice versa. Sample Input: HackerRank.com presents "Pythonist 2".
+Sample Output: hACKERrANK.COM PRESENTS "pYTHONIST 2"."""
+
+text1 = 'HackerRank.com presents "Pythonist 2".'
+text2 = ' Hello My Name is KAVIRAJ DESAI'
+#swapcase() method is used to swap cases in text
+updated_text1=text1.swapcase()
+updated_text2=text2.swapcase()
+print(updated_text1)
+print(updated_text2)
